c50b5cc5f1bb5f27901d6687667cfb1f77f94cc3
Leone0038 <leonedantas80@gmail.com>
2026-03-25 18:07:51 -0300

    add shadow to carousel

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -39,7 +39,7 @@ export default function Carousel() {
     };
     return (
         <section className="flex-1 p-16 grid place-items-center">
-            <div className="rounded-lg shadow w-full max-w-200 md:aspect-video aspect-square relative overflow-hidden">
+            <div className="rounded-lg shadow-xl shadow-neutral-900 w-full max-w-200 md:aspect-video aspect-square relative overflow-hidden">
                 <div
                     className="flex w-full h-full transition duration-500 ease-in-out"
                     style={{
