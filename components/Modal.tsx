ba247c742eb533d7f3d4c0874a3dfcb59942c790
Leone0038 <leonedantas80@gmail.com>
2026-03-25 19:12:40 -0300

    commit

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -21,6 +21,7 @@ export default function Modal({
         setIsAnimate(false);
         setTimeout(() => setShowModal(false), 300);
     };
+    
     return (
         <section
             className={`fixed z-50 p-5 inset-0 grid place-items-center backdrop-blur-sm bg-slate-900/40 transition-opacity duration-300 ease-in-out opacity-${isAnimate ? 100 : 0}`}
