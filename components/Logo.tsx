e74fded79237e530a3c73ba1f451f39cf5ec934a
Leone0038 <leonedantas80@gmail.com>
2026-03-26 13:39:19 -0300

    change logo from client to server component

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,22 +1,18 @@
-"use client";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-import museumLogo from '@/assets/museum-logo.webp'
+import museumLogo from "@/assets/museum-logo.webp";
+import Link from "next/link";
 
 export default function Logo() {
-    const router = useRouter();
-    const redirectToHome = () => {
-        router.push("/");
-    };
     return (
-        <Image
-            src={museumLogo}
-            alt="Logo do museu regional de canindé"
-            priority
-            width={75}
-            height={75}
-            onClick={redirectToHome}
-            className="cursor-pointer"
-        />
+        <Link href={"/"}>
+            <Image
+                src={museumLogo}
+                alt="Logo do museu regional de canindé"
+                priority
+                width={75}
+                height={75}
+                className="cursor-pointer"
+            />
+        </Link>
     );
 }
