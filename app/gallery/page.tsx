607d82d978b31b4da53c2d98e46e29e39ef8d0cf
Leone0038 <leonedantas80@gmail.com>
2026-03-27 19:02:17 -0300

    tweak and remove stuff

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,11 +1,6 @@
 import Carousel from "@/components/Carousel";
 
 export default function GalleryPage() {
-    return (
-        <>
-            <Carousel />
-            <div className="bg-[url(/images/gallery-page-portrait.webp)] md:bg-[url(/images/gallery-page-landscape.webp)] bg-center bg-cover bg-fixed fixed inset-0 -z-10"></div>
-            s
-        </>
-    );
+    return <Carousel />
+    
 }
