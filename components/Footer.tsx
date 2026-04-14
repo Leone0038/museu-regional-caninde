221e7bcb74514f4e97a5042c5809555087f55da5
Leone0038 <leonedantas80@gmail.com>
2026-04-13 21:16:03 -0300

    add year state

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,22 @@
+'use client'
+
+import { useEffect, useState } from "react";
 import Logo from "./Logo";
 
-const currentYear = new Date().getFullYear();
 
 export default function Footer() {
+    const [year, setYear] = useState<number>(0);
+
+    useEffect(() => {
+        const updateYear = () => {
+            setYear(new Date().getFullYear())
+        }
+        updateYear()
+    }, []);
     return (
         <footer className="flex flex-col items-center justify-center p-2 bg-mist-100/90 text-center">
             <Logo />
-            <p>Museu Regional de Canindé - {currentYear}</p>
+            <p>Museu Regional de Canindé - {year}</p>
         </footer>
     );
 }
