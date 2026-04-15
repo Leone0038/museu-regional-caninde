0f03fc5126b44208e0f1b6fb7ad44806a23a03e5
Leone0038 <leonedantas80@gmail.com>
2026-04-15 00:32:56 -0300

    add smooth scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import ScrollToTopBtn from "@/components/ScrollToTopBtn";
 
 export const metadata: Metadata = {
     title: "Museu Regional de Canindé",
@@ -15,11 +16,18 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="pt-BR" className={`h-full antialiased`}>
+        <html
+            lang="pt-BR"
+            className="antialiased scroll-smooth"
+            data-scroll-behavior="smooth"
+        >
             <body className="flex flex-col min-h-dvh">
                 <Header />
                 <main className="flex-1 text-amber-50 lg:p-16 p-8 grid place-items-center">
-                    {children}
+                    <>
+                        <ScrollToTopBtn />
+                        {children}
+                    </>
                 </main>
                 <Footer />
             </body>
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import NavBar from "./NavBar";
 
 export default function Header() {
     return (
-        <header className="flex sm:flex-row flex-col justify-between gap-5 items-center px-8 py-4 bg-amber-100/90 shadow shadow-neutral-600 sticky top-0 z-10">
+        <header className="flex sm:flex-row flex-col justify-between gap-5 items-center px-8 py-4 bg-amber-100/90 shadow shadow-neutral-600">
             <Logo />
             <NavBar />
         </header>
