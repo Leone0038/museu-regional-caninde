4bce8ee122da459daaa879196edbad670c31591d
Leone0038 <leonedantas80@gmail.com>
2026-04-22 15:36:22 -0300

    change logo to take props

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,16 +1,17 @@
 import Image from "next/image";
-import museumLogo from "@/assets/museum-logo.webp";
+import museumLogo from "@/assets/zoo-logo.webp";
 import Link from "next/link";
+import { LogoProps } from "@/types";
 
-export default function Logo() {
+export default function Logo({height, priority,width}: LogoProps) {
     return (
         <Link href={"/"}>
             <Image
                 src={museumLogo}
-                alt="Logo do museu regional de canindé"
-                priority
-                width={75}
-                height={75}
+                alt="Logo do Zoológico de São Francisco - Canindé"
+                priority={priority || false}
+                width={width}
+                height={height}
                 className="cursor-pointer"
             />
         </Link>
