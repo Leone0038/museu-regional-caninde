56435cf316c96aac84936444911f2aa7334478c9
Leone0038 <leonedantas80@gmail.com>
2026-04-22 15:36:57 -0300

    remove link

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,14 +4,19 @@ const links = [
     { href: "/sobre", label: "Sobre" },
     { href: "/contatos", label: "Contate-nos" },
     { href: "/acervo", label: "Acervo" },
-    { href: "/noticias", label: "Notícias" },
 ];
 
 export default function NavBar() {
     return (
         <nav className="flex gap-8 flex-wrap justify-center">
             {links.map(({ href, label }, i) => (
-                <Link className='text-shadow-lg shadow-black' href={href} key={i}>{label}</Link>
+                <Link
+                    className="text-shadow-lg shadow-black"
+                    href={href}
+                    key={i}
+                >
+                    {label}
+                </Link>
             ))}
         </nav>
     );
