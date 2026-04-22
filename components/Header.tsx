3449a771d9c3f77ecab388a0b2c02e7067471b11
Leone0038 <leonedantas80@gmail.com>
2026-04-22 15:36:09 -0300

    add attrs to logo and change font

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,15 @@
+import { fredoka } from "@/app/layout";
 import Logo from "./Logo";
 import NavBar from "./NavBar";
 
 export default function Header() {
     return (
-        <header className="flex sm:flex-row flex-col justify-between gap-5 items-center px-8 py-4 bg-amber-100/90 shadow shadow-neutral-600">
-            <Logo />
+        <header
+            className={`flex sm:flex-row flex-col justify-between gap-5 items-center px-8 py-2 bg-primary shadow shadow-neutral-600 ${
+                fredoka.className
+            }`}
+        >
+            <Logo width={120} height={120} priority={true} />
             <NavBar />
         </header>
     );
