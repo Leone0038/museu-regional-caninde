28e4f2f37c967f2fab1aafc2c13cba4ed990ba9b
Leone0038 <leonedantas80@gmail.com>
2026-04-22 15:35:49 -0300

    change text and add footer styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,22 +1,24 @@
-'use client'
+"use client";
 
 import { useEffect, useState } from "react";
 import Logo from "./Logo";
-
 
 export default function Footer() {
     const [year, setYear] = useState<number>(0);
 
     useEffect(() => {
         const updateYear = () => {
-            setYear(new Date().getFullYear())
-        }
-        updateYear()
+            setYear(new Date().getFullYear());
+        };
+        updateYear();
     }, []);
     return (
-        <footer className="flex flex-col items-center justify-center p-2 bg-mist-100/90 text-center">
-            <Logo />
-            <p>Museu Regional de Canindé - {year}</p>
+        <footer className="flex tracking-wide flex-col items-center justify-center p-2 bg-primary text-center text-xs font-semibold uppercase">
+            <Logo width={90} height={90}/>
+            <p>© {year} - Zoológico de São Francisco - Canindé - CE</p>
+            <p>
+                Rua do Zoológico, s/n - Centro, Canindé, Ceara, Brazil 62700000
+            </p>
         </footer>
     );
 }
