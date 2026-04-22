f7f7385f435538942e4cb01e12643ac254d372da
Leone0038 <leonedantas80@gmail.com>
2026-04-22 15:32:42 -0300

    add logo props type

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,18 @@
-import { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction } from "react";
 
 export interface ModalProps {
     imagePath: string;
     imageIndex: number;
     showModal: boolean
     setShowModal: Dispatch<SetStateAction<boolean>>;
+}
+
+export interface LogoProps {
+    priority?: boolean;
+    width: number
+    height: number
+}
+
+export interface InfoCardProps {
+    children: React.ReactNode
 }
