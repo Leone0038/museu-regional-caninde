4980bf3298b1d993b939039b5c8849c0361b6083
Leone0038 <leonedantas80@gmail.com>
2026-04-27 22:05:08 -0300

    change header bg bolor

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import NavBar from "./NavBar";
 export default function Header() {
     return (
         <header
-            className={`flex sm:flex-row flex-col justify-between gap-5 items-center px-8 py-2 bg-primary shadow shadow-neutral-600 ${
+            className={`flex sm:flex-row flex-col justify-between gap-5 items-center px-8 py-2 bg-amber-100 shadow shadow-neutral-600 ${
                 fredoka.className
             }`}
         >
