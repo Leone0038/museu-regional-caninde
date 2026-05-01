d1dc485b0be43dcfdf110aebd6c933baf86e9f3e
Leone0038 <leonedantas80@gmail.com>
2026-04-30 21:08:23 -0300

    add template numbers

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -5,13 +5,13 @@ export default function ContactsPage() {
         <main className="flex-1">
             <section className="flex flex-col gap-8 text-center font-bold">
                 <InfoCard>
-                    <p></p>
+                    <p>(63) 93633-4186</p>
                 </InfoCard>
                 <InfoCard>
-                    <p></p>
+                    <p>(88) 2586-7765</p>
                 </InfoCard>
                 <InfoCard>
-                    <p></p>
+                    <p>(63) 92283-3244</p>
                 </InfoCard>
             </section>
         </main>
